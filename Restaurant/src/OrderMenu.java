ae36c48367ec29709e640b252cc0c898abc1e4e3
Le Tony <le.tony77340@gmail.com>
2019-02-08 10:09:25 +0100

    cours

---
+++ Restaurant/src/OrderMenu.java
@@ -0,0 +1,12 @@
+public class OrderMenu {
+
+    public static void main(String[] args) {
+        
+        MenuNeufEuros order = new  MenuNeufEuros();
+        order.RunMenus();
+               /* Order order = new Order();
+                //order.runMenu();
+                order.runMenus();*/
+            }
+
+}
